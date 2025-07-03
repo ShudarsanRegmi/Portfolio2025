d0cf98abe47aa74100a53b6f3fcb12369acb5e6f
Shudarsan Regmi <shudarsanregmi555@gmail.com>
2025-07-03 18:47:36 +0530

    chanted application layout: updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import { render } from 'preact'
-import './index.css'
-import { App } from './app.tsx'
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-render(<App />, document.getElementById('app')!)
+createRoot(document.getElementById("root")!).render(<App />);
