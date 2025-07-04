4fcc852b0e9c6988a84245e10d94648670c45414
Shudarsan Regmi <shudarsanregmi555@gmail.com>
2025-07-04 18:29:17 +0530

    Minimal Certification Section was added

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Hero from "@/pages/Hero";
 import About from "@/pages/About";
 import Projects from "@/pages/Projects";
 import TechStack from "@/pages/TechStack";
-// import Certifications from "@/pages/Certifications";
+import Certifications from "@/pages/Certifications";
 // import Experience from "@/pages/Experience";
 // import Achievements from "@/pages/Achievements";
 // import Contact from "@/pages/Contact";
@@ -25,7 +25,7 @@ function Router() {
         <Route path="/about" component={() => <PageTransition><About /></PageTransition>} />
         <Route path="/projects" component={() => <PageTransition><Projects /></PageTransition>} />
         <Route path="/tech" component={() => <PageTransition><TechStack /></PageTransition>} />
-        {/* <Route path="/certifications" component={() => <PageTransition><Certifications /></PageTransition>} /> */}
+        <Route path="/certifications" component={() => <PageTransition><Certifications /></PageTransition>} />
         {/* <Route path="/experience" component={() => <PageTransition><Experience /></PageTransition>} /> */}
         {/* <Route path="/achievements" component={() => <PageTransition><Achievements /></PageTransition>} /> */}
         {/* <Route path="/contact" component={() => <PageTransition><Contact /></PageTransition>} /> */}
